2207b84c90f900eff056fe1a4556bd5a689f7411
sekine104 <sekine104@outlook.jp>
2021-06-02 00:05:16 +0900

    a-

--- classes/battle.py
+++ classes/battle.py
@@ -1,9 +1,15 @@
-
 from classes.character import Character
 
 
 class BattleEngine:
-    pass
+    def __init__(self) -> None:
+        self.logics = {}
+
+    def add(self, key, value=None):
+        self.logics.setdefault(key, value)
+
+    def get(self, key):
+        return self.logics.get(key)
 
 class BattleLogic:
     def __init__(self) -> None:
@@ -18,6 +24,7 @@ class BattleLogic:
 class SingleBattleLogic(BattleLogic):
     def __init__(self, player: Character, enemy: Character) -> None:
         super().__init__()
+        self._type = "Single"
         self.player = player
         self.enemy = enemy
 
@@ -28,3 +35,6 @@ class SingleBattleLogic(BattleLogic):
                 self._stop()
             print("LOOPING")
 
+
+if __name__ == "__main__":
+    pass
