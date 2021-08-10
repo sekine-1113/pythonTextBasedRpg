615b357eb8a66553dfaf42ced8e3339477746210
sekine104 <sekine104@outlook.jp>
2021-08-10 22:48:19 +0900

    update

--- game/settings.py
+++ game/settings.py
@@ -4,22 +4,35 @@
 
 from hashlib import sha256
 from getpass import getpass
+import random
+import string
 
 
 user = {
     "name": "Bob",
+    "id": "",
     "password": ""
 }
 
-password = ""
 
-while len(password) < 4:
-    password = getpass()
-    print(password)
-    a = sha256(password.encode()).hexdigest()
-    print(a)
-
-user["password"] = a
-print(user["password"])
+def make_id():
+    k1 = "".join(random.choices(string.ascii_uppercase, k=2))
+    k2 = "".join(random.choices(string.digits, k=4))
+    user_id = k1 + k2
+    print(user_id)
+    return user_id
 
 
+def make_pass():
+    password = ""
+
+    while len(password) < 4:
+        password = getpass()
+    user_password = sha256(password.encode()).hexdigest()
+    print(user_password)
+    return user_password
+
+
+print("ID:")
+make_id()
+make_pass()
