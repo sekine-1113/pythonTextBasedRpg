829c8a6e9d37852f32ce0da2e59a9ce96fa70298
sekine104 <sekine104@outlook.jp>
2021-08-19 00:29:18 +0900

    update

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import os, sys
 sys.path.append(os.path.dirname(sys.argv[0]))
 
-print("")
+import unittest
+
+
+class TestSample(unittest.TestCase):
+
+    def test_add_equal(self):
+        a, b = 2, 5
+        c = 7
+        self.assertEqual(a+b, c)
+
+    def test_add_true(self):
+        a, b = 2, 5
+        c = 7
+        self.assertTrue(a+b==c)
+        # false
+        # self.assertFalse()
+
+    def test_add_instance(self):
+        a,b = 2,5
+        self.assertIsInstance(a+b, int)
+
+
+if __name__ == "__main__":
+    unittest.main()
