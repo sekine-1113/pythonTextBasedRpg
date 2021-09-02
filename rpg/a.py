020c0216304c7d2c5f31efe343e43b1355d618ad
sekine104 <sekine104@outlook.jp>
2021-09-03 03:12:31 +0900

    Update

--- rpg/a.py
+++ rpg/a.py
@@ -1,30 +1,66 @@
 
 class BaseItem:
-    def __init__(self, name: str, price: int) -> None:
+    def __init__(self, name: str, price: int, _id: int) -> None:
         self.name = name
         self.price = price
+        self._id = _id
 
 
 class Item(BaseItem):
-    def __init__(self, name: str, price: int) -> None:
-        super().__init__(name, price)
+    def __init__(self, name: str, price: int, _id: int) -> None:
+        super().__init__(name, price, _id)
 
     def show(self) -> None:
-        print(self.name)
+        print(self._id, self.name)
         print(self.price)
 
     def clone(self) -> "Item":
-        return Item(self.name, self.price)
+        return Item(self.name, self.price, self._id)
 
+    def __repr__(self) -> str:
+        return f"{self._id}: {self.name}"
+
+
+class ItemList:
+    def __init__(self) -> None:
+        self.strage = []
+
+    def add(self, item: Item) -> None:
+        self.strage.append(item)
+
+    def remove(self, item: Item) -> None:
+        self.strage.remove(item)
+
+    def show(self) -> None:
+        for strage in self.strage:
+            print(strage)
+
+    def sort(self, key: object=None, reverse: bool=False) -> None:
+        if key is None:
+            key = lambda x: x._id
+        self.strage.sort(key=key, reverse=reverse)
 
 
 if __name__ == "__main__":
 
-    item = Item("やくそう", 8)
+    item = Item("やくそう", 8, 1)
     item2 = item.clone()
 
-    print(item.name, item2.name)
-
     item.name = "上やくそう"
+    item._id = 2
 
-    print(item.name, item2.name)
+    item_list = ItemList()
+    item_list.add(item)
+    item_list.add(item2)
+
+    item_list.show()
+
+    print("===========")
+    item_list.sort()
+
+    item_list.show()
+    print("===========")
+
+    item_list.remove(item)
+
+    item_list.show()
