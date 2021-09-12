470ef08b29ac27cc4c4928b3eb313a4f13c99647
sekine104 <sekine104@outlook.jp>
2021-09-12 21:47:05 +0900

    update

--- battle.py
+++ battle.py
@@ -1,12 +1,20 @@
 print("Battle")
 # a-a
 
+class DefaultClass:
+    def __init__(self) -> None:
+        print(self.__class__.__name__)
+
 class ClassA:
     def __init__(self) -> None:
         print(self.__class__.__name__)
 
 
-ClassA()
-className = None
-className = input("> ")
+class_dict = {
+    "classa": ClassA
+}
+className = input("> ").lower()
+cls = class_dict.get(className, DefaultClass)
+
+cls()
 
