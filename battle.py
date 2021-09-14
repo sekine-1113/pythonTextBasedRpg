1d968f243be30952fe0e4ab1505e92f0a4e70bd5
sekine104 <sekine104@outlook.jp>
2021-09-14 10:28:34 +0900

    update

--- battle.py
+++ battle.py
@@ -1,22 +1,71 @@
-print("Battle")
-# a-a
 
-class DefaultClass:
-    def __init__(self, **obj) -> None:
-        print(self.__class__.__name__)
-        self.obj = obj
-
-class ClassA:
-    def __init__(self, **obj) -> None:
-        print(self.__class__.__name__)
-        self.obj = obj
+class Debuff:
+    pass
 
 
-class_dict = {
-    "classa": ClassA
-}
-className = input("> ").lower()
-cls = class_dict.get(className, DefaultClass)
-obj = {}
-cls(**obj)
+class Poison(Debuff):
+    def __init__(self) -> None:
+        self.damage = 5
+        self.turn = 3
+
+    def effect(self):
+        print("毒の効果!")
+        print(self.turn)
+        self.turn -= 1
+        if self.turn == 0:
+            print("毒は消えた!")
+
+
+
+class Sleep(Debuff):
+    def __init__(self) -> None:
+        self.damage = 0
+        self.turn = 3
+
+    def effect(self):
+        print("眠りの効果!")
+        self.turn -= 1
+        if self.turn == 0:
+            print("眠りは消えた!")
+
+
+class Player:
+    def __init__(self) -> None:
+        self.name = "Alice"
+        self.debuff = []
+        self.hp = 32
+
+    def action(self):
+        for debuff in self.debuff:
+            if debuff.turn == 0:
+                self.debuff.remove(debuff)
+                continue
+            debuff.effect()
+
+
+class Enemy:
+    def __init__(self) -> None:
+        self.name = "Slime"
+        self.debuff = []
+        self.hp = 16
+
+    def action(self):
+        for debuff in self.debuff:
+            debuff.effect()
+
+
+
+player = Player()
+enemy = Enemy()
+poison = Poison()
+player.debuff.append(poison)
+
+while True:
+    player.action()
+    if enemy.hp <= 0:
+        break
+    player.hp -= 8
+    if player.hp <= 0:
+        break
+
 
