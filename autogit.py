f917d20d50712b753d598113676743b7d7d57f6e
sekine104 <sekine104@outlook.jp>
2021-09-22 12:56:55 +0900

    auto

--- autogit.py
+++ autogit.py
@@ -1,18 +1,7 @@
 import os
-import sys
-
-def split(x, s="="):
-    return x.split("=")
 
 
-if sys.argv[1:]:
-    comments = list(map(split, sys.argv[1:]))
-    for i, cmt in enumerate(comments):
-        if "message" in cmt:
-            comments[i].remove("message")
-            comment = comments[i][0]
-else:
-    comment = input("Git commit message >> ")
+comment = input("Git commit message >> ")
 
 git_add = "git add ."
 git_commit = "git commit -m {}"
