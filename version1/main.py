8f7411c4b8946f3598c81198ffca49d4004301b0
sekine104 <sekine104@outlook.jp>
2021-10-19 19:57:04 +0900

    update

--- version1/main.py
+++ version1/main.py
@@ -1,7 +1,17 @@
-import os
-import sys
+from version1.system.character import *
 
+def print_profile():
+    print( "____________________")
+    print( "|==================|")
+    print( "|** Your Profile **|")
+    print( "|==================|")
+    print(f"|LEVEL   : {PLAYER_LEVEL:8}|")
+    print(f"|HITPOINT: {PLAYER_HITPOINT:8}|")
+    print(f"|POWER   : {PLAYER_POWER:8}|")
+    print( "￣￣￣￣￣￣￣￣￣￣")
 
-print(sys.argv[0])
+PLAYER_NAME = "Alice"
+ENEMY_NAME = "Slime"
 
-print("hello world! ")
+print(f"Welcome to TextBasedRpg, {PLAYER_NAME}!")
+print_profile()
--- version1/system/battle.py
+++
@@ -1,9 +0,0 @@
-
-def fight(player, enemy):
-    print(player.name, enemy.name)
-    player.action()
-    enemy.action()
-
-
-if __name__ == "__main__":
-    pass
