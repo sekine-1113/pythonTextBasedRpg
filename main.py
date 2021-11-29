4afc8d990e801f8fd0b2d910d991d02ee48076a0
sekine104 <sekine104@outlook.jp>
2021-11-29 23:21:05 +0900

    update

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("run")
+from utils.windows import Color, unlock_ansi
+
+unlock_ansi()
+
+print(f"{Color.BLUE}[RED]{Color.RESET}")
+
+class Printer:
+    def __init__(self, color) -> None:
+        self.color = color
+
+    def __enter__(self):
+        if self.color is None:
+            return self
+        print(self.color, end="")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print(Color.RESET, end="")
+
+
+with Printer(Color.RED) as p:
+    print("Hello")
