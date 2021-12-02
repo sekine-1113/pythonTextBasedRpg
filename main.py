674a8ef7ff0aca32a15e8ba330f0d1a2b84100fb
sekine104 <sekine104@outlook.jp>
2021-12-02 13:46:36 +0900

    update

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ from utils.windows import Color, unlock_ansi
 
 unlock_ansi()
 
-print(f"{Color.BLUE}[RED]{Color.RESET}")
 
 class Printer:
-    def __init__(self, color) -> None:
+    def __init__(self, color=None) -> None:
         self.color = color
 
     def __enter__(self):
         if self.color is None:
             return self
         print(self.color, end="")
+        return self
+
+    def red(self, txt,  **kwargs):
+        print(Color.RED+txt, **kwargs)
+        return self
+
+    def green(self, txt, **kwargs):
+        print(Color.GREEN+txt, **kwargs)
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         print(Color.RESET, end="")
 
 
-with Printer(Color.RED) as p:
-    print("Hello")
+with Printer() as p:
+    p.red("Hello", end="").green("World")
--- tmp/utility/klass.py
+++ tmp/utility/klass.py
@@ -4,6 +4,49 @@ class Dictable:
     def asdict(self):
         r = {}
         for k, v in vars(self).items():
-            if isinstance(v, Dictable): v = v.asdict()
+            if isinstance(v, Dictable):
+                v = v.asdict()
             r[k] = v
         return r
+
+
+class MyClass(object):
+    def __init__(self) -> None:
+        self._object = {}
+
+    def bind(self, key, value):
+        self.__setattr__(key, value)
+        self.__setitem__(key, value)
+
+    def __getitem__(self, key):
+        return self._object.get(key)
+
+    def __setitem__(self, key, value):
+        self._object[key] = value
+        self.__setattr__(key, value)
+        return self
+
+    def __getattr__(self, key):
+        return self._object.get(key)
+
+    def __setattr__(self, __name: str, __value) -> None:
+        super().__setattr__(__name, __value)
+        self._object.setdefault(__name, __value)
+
+
+def TestMyClass():
+    class Inner:
+        def method(self): return "method"
+    mycls = MyClass()
+    mycls.bind("value", 10)
+    assert mycls["value"] == mycls.value
+    mycls.bind("add", lambda x,y: x+y)
+    assert mycls["add"](3, 7) == mycls.add(4, 6)
+    mycls["inner"] = Inner()
+    assert mycls["inner"].method() == mycls.inner.method()
+    mycls.method = Inner().method
+    assert mycls["method"]() == mycls.method()
+    print("OK")
+
+if __name__ == "__main__":
+    TestMyClass()
