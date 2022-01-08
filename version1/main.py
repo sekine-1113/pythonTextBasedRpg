c598f000aa0401132a6b0af108929f4785b89051
sekine104 <sekine104@outlook.jp>
2022-01-08 19:21:36 +0900

    update

--- version1/main.py
+++ version1/main.py
@@ -9,15 +9,30 @@ class Player:
     def __repr__(self) -> str:
         return f"[#{self.idx}] {self.name} {self.money}G (#{self.class_id})"
 
+class Enemy:
+    def __init__(self, idx, name, money) -> None:
+        self.idx = idx
+        self.name = name
+        self.money = money
+
+    def __repr__(self) -> str:
+        return f"[#{self.idx}] {self.name} {self.money}G"
+
 
 class ActorFactory:
     def __init__(self, actor_class) -> None:
         self.actor_class = actor_class
 
     def create(self):
-        return self.actor_class(1, "アリス", 0, 1)
+        if issubclass(self.actor_class, Player):
+            return self.actor_class(1, "アリス", 0, 1)
+        else:
+            return self.actor_class(1, "ゴブリン", 0)
 
 
 player_factory = ActorFactory(Player)
 player = player_factory.create()
+enemy_factory = ActorFactory(Enemy)
+enemy = enemy_factory.create()
 print(player)
+print(enemy)
