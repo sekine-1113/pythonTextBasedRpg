09ed407e0d0562173606114d42b4961eeefa2457
sekine104 <sekine104@outlook.jp>
2022-03-19 04:59:15 +0900

    Update

--- simpleRPG2/main.py
+++ simpleRPG2/main.py
@@ -9,113 +9,118 @@ from simpleRPG2 import (
     integer,
 )
 
+import json
 
-import json, os
-
-def test_loader():
-    class Loader:
-        def __init__(self, base_dir=""):
-            self._base_dir = base_dir
-
-        def loads(self, data):
-            return json.loads(data)
-
-        def load(self, path):
-            file = os.path.join(
-                self._base_dir,
-                path)
-            with open(file, "r") as f:
-                return json.load(f)
-
-
-    data = '{"players":[]}'
-    loader = Loader("./simpleRPG2/")
-    print(loader.loads(data))
-    # print(loader.load("players.json"))
 
 
 def test_player_manager():
-    save = {
-        "players":
-            [
-                {
-                    "id": 0,
-                    "name": "alice"
-                },
-                {
-                    "id": 1,
-                    "name": "bob"
-                },
-                {
-                    "id": 2,
-                    "name": "stefan"
-                }
-            ]
 
+    save = {
+        "players": [
+            {
+                "id": 0,
+                "name": "alice",
+            },
+            {
+                "id": 1,
+                "name": "bob"
+            },
+            {
+                "id": 2,
+                "name": "stefan"
+            }
+        ]
     }
 
     class PlayerManager:
-        def __init__(self, players):
+        def __init__(self, players: list) -> None:
             self.__players = players
 
-        def get(self, _id):
+        def get(self, _id: int) -> dict|None:
             for player in self.__players:
                 if player.get("id") == _id:
                     return player
-            return
 
-        def add(self, player):
+        def add(self, player: dict) -> None:
             self.__players.append(player)
 
-        def make(self, name, _id=None):
-            if _id is not None:
+        def get_usable_ids(self) -> list[int]:
+            i: int
+            usable_ids: list[int] = []
+            for i in range(self.size()+1):
+                if self.get(i) is None:
+                    usable_ids.append(i)
+            return usable_ids
+
+        def make(self, name: str, _id: int=None) -> None:
+            usable_ids: list[int] = self.get_usable_ids()
+            if _id is not None and _id in usable_ids:
                 self.add({
                     "id": _id,
                     "name": name
                 })
                 return
-            usable_id = []
-            for i in range(self.size()+1):
-                if self.get(i) is None:
-                    usable_id.append(i)
-            char_id = min(usable_id)
+
+            char_id: int = min(usable_ids)
             self.add({
                 "id": char_id,
                 "name": name
             })
 
-
-        def dumps(self):
+        def dumps(self) -> None:
+            player: dict
             for player in self.__players:
                 print(player)
 
-        def size(self):
+        def size(self) -> int:
             return len(self.__players)
 
-        def clear(self):
+        def clear(self) -> None:
             self.__players = []
 
-        def sort(self):
-            pass
+        def sort(self) -> None:
+            self.__players.sort(key=lambda x: x["id"])
+
+        def output(self) -> dict:
+            return {"players": self.__players}
 
 
-
-    players = save.get("players")
-    player_manager = PlayerManager(players)
+    player_manager = PlayerManager(save.get("players"))
+    print(f"{player_manager.output()=}")
     player_manager.clear()
     if (0 == player_manager.size()):
         player_manager.make("peach")
-    player = deepcopy(player_manager.get(0))
-    print(player)
-    player["id"] = 3
-    player["name"] = "bes"
-    player_manager.add(player)
+    player_manager.add({"id": 5, "name":"mike"})
     player_manager.make("jack")
-    player_manager.make("bob", 5)
-    print(player_manager.get(3))
+    player_manager.make("bob")
+    player_manager.make("wendy", 1)
+    print(f"{player_manager.get(3)=}")
+    player_manager.sort()
     player_manager.dumps()
+
+    print(f"{player_manager.output()=}")
+
+
+    class FileManager:
+        def __init__(self, base_dir=""):
+            self._base_dir = base_dir
+
+        def load(self, path: str) -> dict:
+            file = os.path.join(
+                self._base_dir,
+                path)
+            with open(file, "r", encoding="UTF-8") as f:
+                return json.load(f)
+
+        def save(self, path: str, data: object) -> None:
+            file: str = os.path.join(self._base_dir, path)
+            with open(file, "w", encoding="UTF-8") as f:
+                json.dump(data, f, indent=4)
+
+
+    # fm = FileManager("./simpleRPG2")
+    # fm.save("data/players.json", player_manager.output())
 
 
 if __name__ == "__main__":
-    test_loader()
     test_player_manager()
