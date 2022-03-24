9dfab7be5b9d1419a2208bc7cea38ed36b0fd5ae
sekine104 <sekine104@outlook.jp>
2022-03-24 20:04:17 +0900

    Update

--- simpleRPG2/tests/to_json.py
+++ simpleRPG2/tests/to_json.py
@@ -105,29 +105,38 @@ class Users:
     def __init__(self, users: list[User]) -> None:
         self.users = users
 
-def save(json_str, file, encoding="UTF-8"):
+def save(json_str: str, file: str, encoding: str="UTF-8"):
     with open(file, "w", encoding=encoding) as f:
         json.dump(json_str, f, indent=4)
 
-save(
-    tojson(
-        Users(
-            users=[
-                User(name="Alice", age=27, items=[Item(name="iPhone12")]),
-                User(name="Bob", age=30, items=[Item(name="iPhone8"),Item(name="iPhone10")])
-            ]
-        )
-    ),
-    r"simpleRPG2\tests\test.json"
+
+def load(file, encoding: str="UTF-8"):
+    with open(file, "r", encoding=encoding) as f:
+        return json.load(f)
+
+
+file = r"simpleRPG2\tests\test.json"
+
+json_str = tojson(
+    Users(
+        users=[
+            User(name="Alice", age=27, items=[Item(name="iPhone12")]),
+            User(name="Bob", age=30, items=[Item(name="iPhone8"),Item(name="iPhone10")])
+        ]
+    )
 )
 
+save(json_str, file)
+load_json_str = load(file)
+users = []
+for user in load_json_str['users']:
+    users.append(User(user['name'], user['age'], [Item(item['name']) for item in user['items']]))
 
+print(f"{json_str == tojson(Users(users)) = }")
 """
-{"Users":
-    {"data": [
-            {"User": {"name": "Alice", "age": 27, "items": [{"Item": {"name": "iPhone12"}}]}},
-            {"User": {"name": "Bob", "age": 30, "items": [{"Item": {"name": "iPhone8"}}, {"Item": {"name": "iPhone10"}}]}}
-        ]
-    }
+{"Users": [
+        {"User": {"name": "Alice", "age": 27, "items": [{"Item": {"name": "iPhone12"}}]}},
+        {"User": {"name": "Bob", "age": 30, "items": [{"Item": {"name": "iPhone8"}}, {"Item": {"name": "iPhone10"}}]}}
+    ]
 }
 """
