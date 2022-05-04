44a2d88d9ad4559d3bb170b4de67397dc22147a4
sekine104 <sekine104@outlook.jp>
2022-05-05 05:09:28 +0900

    update

--- betasimpleRPG3/downloader.py
+++ betasimpleRPG3/downloader.py
@@ -1,6 +1,19 @@
 import requests
+import zipfile
+import os
 
-URL = "https://raw.githubusercontent.com/alice-1113/pythonTextBasedRpg/main/betasimpleRPG2/data/players.json"
-res = requests.get(URL).json()
 
-print(res)
+
+
+if False:
+    URL = r"https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-large-zip-file.zip"
+    res = requests.get(URL)
+    with open("example.zip", "wb") as f:
+        f.write(res.content)
+    # ----- ^ download ---------
+    # ----- ↓ unpack -----------
+    with zipfile.ZipFile("example.zip") as f:
+        f.extractall("example_zip_unpacks")
+
+    os.remove("")
+
