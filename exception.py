82ef734092a3aa043912b33c4fba5cae8e710e91
sekine104 <sekine104@outlook.jp>
2022-05-10 00:28:52 +0900

    update

--- exception.py
+++ exception.py
@@ -1,53 +1,45 @@
 from pprint import pprint
 
 
-def t_leven(s1, s2):
-    if not s1: return len(s2)
-    if not s2: return len(s1)
-    if s1[0] == s2[0]: return t_leven(s1[1:], s2[1:])
+def levenshtein(s: str, t: str) -> int:
+    s = s.lower()
+    t = t.lower()
 
-    l1 = t_leven(s1, s2[1:])
-    l2 = t_leven(s1[1:], s2)
-    l3 = t_leven(s1[1:], s2[1:])
-    return 1 + min(l1, l2, l3)
-def t_lss(s1, s2):
-    return (-t_leven(s1, s2)) / max(len(s1),len(s2)) + 1
-
-# レーヴェンシュタイン距離
-def lev(s, t):
     ls = len(s)
     lt = len(t)
-
     if not s: return lt
     if not t: return ls
 
-    dp = [[0]*(lt+1)]*(ls+1)
+    dp = [ [0]*(lt+1) for _ in range(ls+1) ]
+    for i in range(1, ls+1):
+        dp[i][0] = i
     for j in range(1, lt+1):
         dp[0][j] = j
-
     for i in range(1, ls+1):
         dp[i][0] = i
         for j in range(1, lt+1):
             cost = not s[i-1] == t[j-1]
             dp[i][j] = min(
+                dp[i-1][j-1] + cost,
                 dp[i-1][j] + 1,
-                dp[i][j-1] + 1,
-                dp[i-1][j-1] + cost)
+                dp[i][j-1] + 1
+            )
     return dp[-1][-1]
 
-def lss(s, t):
-    m = max(len(s), len(t))
-    v = lev(s, t)
-    return 1 - (v / m)
+def normalize(s, t):
+    return 1 - levenshtein(s, t) / max(len(s), len(t))
 
 
-# tst = [
-#     ("ABC", ""),
-#     ("", "ABC"),
-#     ("ABC", "ABC"),
-#     ("ABC", "BCD"),
-#     ("ABC", "A")]
-# for t in tst:
-#     print(f"{t=} {t_lss(*t)=:.4} {lss(*t)=:.4}")
+allow_str = ["hello", "world", "good", "python"]
+while True:
+    user_input = input(">> ")
+    if user_input in allow_str:
+        print("入力値正常")
+    else:
+        maybe = []
+        for s in allow_str:
+            n = normalize(user_input, s)
+            if n >= 0.65:
+                maybe.append(s)
+        print("もしかして", maybe, "じゃない?")
 
-lev("かがわ", "かながわ")
