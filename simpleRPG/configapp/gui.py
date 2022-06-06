8d05c462568a55ae4e1585dc81a4410b875e83d1
sekine104 <sekine104@outlook.jp>
2022-06-06 13:19:26 +0900

    Update

--- simpleRPG/configapp/gui.py
+++ simpleRPG/configapp/gui.py
@@ -4,6 +4,8 @@ import PySimpleGUI as sg
 layout = [
     [sg.T("Hello!"), sg.T("", size=(20, 1), key="-name-")],
     [sg.T("Input your name: "), sg.I(key="-nameinput-")],
+    [sg.T("Your selection Folder: "), sg.T("", size=(30, 1), key="-folder-")],
+    [sg.FolderBrowse(key="-selectionFolder-")],
     [sg.Button("Yes")]
 ]
 
@@ -13,7 +15,9 @@ while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED:
         break
-    window["-name-"].update(values["-nameinput-"],text_color="red")
+    window["-name-"].update(values["-nameinput-"])
+    window["-folder-"].update(values["-selectionFolder-"])
+    print(values)
 
 window.close()
 
