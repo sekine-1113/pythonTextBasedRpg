d368f833097821b1fd14e8e337ccce3817f51faf
sekine104 <sekine104@outlook.jp>
2022-06-14 10:11:18 +0900

    Update

--- simpleRPG/configapp/gui.py
+++ simpleRPG/configapp/gui.py
@@ -6,7 +6,7 @@ layout = [
     [sg.T("Input your name: "), sg.I(key="-nameinput-")],
     [sg.T("Your selection Folder: "), sg.T("", size=(30, 1), key="-folder-")],
     [sg.FolderBrowse(key="-selectionFolder-")],
-    [sg.Button("Yes")]
+    [sg.Button("Update", key="-update-"), sg.Button("Write", key="-write-")]
 ]
 
 window = sg.Window("Notitle", layout)
@@ -18,6 +18,9 @@ while True:
     window["-name-"].update(values["-nameinput-"])
     window["-folder-"].update(values["-selectionFolder-"])
     print(values)
+    if event == "-write-":
+        print("Write,", values["-nameinput-"], values["-selectionFolder-"])
+        sg.Popup("Write a cui.")
 
 window.close()
 
