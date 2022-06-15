a524964f9c78fba1ca6ee9f7ea7eacccfa37e5c2
sekine104 <sekine104@outlook.jp>
2022-06-15 10:05:55 +0900

    update

--- simpleRPG/configapp/gui.py
+++ simpleRPG/configapp/gui.py
@@ -1,26 +1,39 @@
 import PySimpleGUI as sg
 
 
+
+class EventHandlers:
+    Update = "-update-"
+    Write = "-write-"
+
 layout = [
     [sg.T("Hello!"), sg.T("", size=(20, 1), key="-name-")],
     [sg.T("Input your name: "), sg.I(key="-nameinput-")],
-    [sg.T("Your selection Folder: "), sg.T("", size=(30, 1), key="-folder-")],
-    [sg.FolderBrowse(key="-selectionFolder-")],
-    [sg.Button("Update", key="-update-"), sg.Button("Write", key="-write-")]
+    [sg.T("Your selection Folder: "), sg.T("", size=(30, 1), key="-folder-"), sg.FolderBrowse(key="-selectionFolder-")],
+    [sg.Button("Update", key=EventHandlers.Update), sg.Button("Write", key=EventHandlers.Write)]
 ]
 
 window = sg.Window("Notitle", layout)
 
+
+def update_event(values):
+    window["-name-"].update(values["-nameinput-"])
+    window["-folder-"].update(values["-selectionFolder-"])
+
+def write_event(values):
+    print("Write,", values["-nameinput-"], values["-selectionFolder-"])
+
+
+event_handler = {
+    EventHandlers.Update: update_event,
+    EventHandlers.Write: write_event
+}
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED:
         break
-    window["-name-"].update(values["-nameinput-"])
-    window["-folder-"].update(values["-selectionFolder-"])
-    print(values)
-    if event == "-write-":
-        print("Write,", values["-nameinput-"], values["-selectionFolder-"])
-        sg.Popup("Write a cui.")
+    event_handler.get(event)(values)
+
 
 window.close()
 
