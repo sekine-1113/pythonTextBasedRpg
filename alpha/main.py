99791b7333b8f6d550f06590e8e3bd1cba57e874
sekine104 <sekine104@outlook.jp>
2022-12-28 06:41:53 +0900

    update

--- alpha/main.py
+++ alpha/main.py
@@ -1,18 +1,21 @@
-from typing import TypeVar
-
-
-Name = TypeVar("Name", str)
+from typing import Generic, TypeVar
 
 
 class Stats:
-    def __init__(self, HP, STR, DEF) -> None:
+    def __init__(self, HP: int, STR: int, DEF: int) -> None:
+        assert isinstance(HP, int)
+        assert isinstance(STR, int)
+        assert isinstance(DEF, int)
         self.HP = HP
         self.STR = STR
         self.DEF = DEF
+        assert isinstance(self.HP, int)
+        assert isinstance(self.STR, int)
+        assert isinstance(self.DEF, int)
 
 
 class Actor:
-    def __init__(self, name: Name, stats: Stats) -> None:
+    def __init__(self, name: str, stats: Stats) -> None:
         self.name = name
         self.stats = stats
 
@@ -24,7 +27,7 @@ class Actor:
         return self.stats.HP <= 0
 
     def __repr__(self) -> str:
-        return f"{self.__id} {self.name}"
+        return f"{self.name}"
 
 
 
@@ -33,7 +36,6 @@ def onTurn(player: Actor, enemy: Actor):
     enemy.onTurn()
 
 def battle(player: Actor, enemy: Actor):
-    print(player, enemy)
     while not player.isDead() or enemy.isDead():
         onTurn(player, enemy)
         player.stats.HP -= 10
@@ -47,8 +49,8 @@ def battle(player: Actor, enemy: Actor):
 
 
 def main():
-    player = Actor(0, "a", Stats(200, 100, 100))
-    enemy = Actor(1, "b", Stats(200, 20, 20))
+    player = Actor("a", Stats(200, 100, 100))
+    enemy = Actor("b", Stats(200, 20, 20))
     battle(player, enemy)
 
     return 0
