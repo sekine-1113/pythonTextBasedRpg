55445ed11b1ef3680f55c46bfd02178e51d3f1fe
sekine104 <sekine104@outlook.jp>
2023-02-14 12:09:46 +0900

    update

--- alpha/interface/_command.py
+++
@@ -1,8 +0,0 @@
-from games.cui.textbasedrpg.alpha.core import ABC, abstractmethod
-
-
-class _Command(ABC):
-
-    @abstractmethod
-    def execute(self):
-        pass
---
+++ sample/SQL.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+sql = sqlite3.connect(":memory:")
+cur = sql.cursor()
+
+# cur.execute("CREATE TABLE users (ID int);")
+
+
+
+
