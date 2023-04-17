eac65abcdec690e899a23dfb2a6981401a47d2a7
sekine104 <sekine104@outlook.jp>
2023-04-18 00:51:31 +0900

    Party

--- main.py
+++ main.py
@@ -1,154 +1,143 @@
-from abc import ABC, abstractmethod
-
-
-class Stat:
-    def __init__(self, value):
-        self.value = max(0, value)
-
-    def update(self, amount):
-        self.value = max(0, self.value - amount)
-
-
-class HP(Stat):
-    pass
-
-
-class Attack(Stat):
-    pass
-
+import random
 
 
 class Actor:
-    def __init__(self, name, hp, atk, level=1, exp=0) -> None:
+    def __init__(self, name, level, hit_point, strength, defense) -> None:
         self.name = name
-        self.hp = hp
-        self.atk = atk
         self.level = level
-        self.exp = exp
-
-    def print_status(self) -> None:
-        print(f"{self.name} HP:{self.hp.value} ATK:{self.atk.value} EXP:{self.exp}")
-
-    def is_dead(self) -> bool:
-        return self.hp.value <= 0
+        self.max_hit_point = hit_point
+        self.hit_point = hit_point
+        self.strength = strength
+        self.defense = defense
 
     def attack(self, target):
-        self.show_attack_message(target, self.atk.value)
-        target.hp.update(self.atk.value)
+        damage = self.strength + random.randint(0, int(self.strength / 4))
+        at_damage = target.receive(damage)
+        print(f"{at_damage}ダメージあたえた!")
 
-    def show_attack_message(self):
-        pass
+    def receive(self, damage):
+        damage = damage - self.defense + random.randint(0, 1)
+        damage = abs(damage)
+        print(f"{self.name}は{damage}ダメージを受けた")
+        self.hit_point -= damage
+        if self.hit_point < 0:
+            self.hit_point = 0
+        return damage
 
-    def clone(self):
-        pass
+    def reset(self):
+        self.hit_point = self.max_hit_point
 
-    def on_turn_end(self):
-        pass
-
-
-
-
-
-class Player(Actor):
-    def __init__(self, name, hp, atk) -> None:
-        super().__init__(name, hp, atk)
-
-    def show_attack_message(self, target, dmg):
-        print(self.name, "の", "こうげき")
-        print(target.name, "に", dmg, "ダメージを与えた!")
-
-    def clone(self) -> "Player":
-        return Player(self.name, self.hp, self.atk)
-
-    def on_turn(self, target):
-        command = int(input("[1]Attack [0]Run\n> "))
-        match command:
-            case 0:
-                print("にげた!")
-                return False
-            case 1:
-                self.attack(target)
-        return True
-
-
-
-    def earn_exp(self, exp):
-        self.exp += exp
-        if self.exp >= 30000:
-            self.exp = self.exp % 30000
-            self.level_up()
-
+    def is_dead(self):
+        return self.hit_point <= 0
 
     def level_up(self):
-        print(f"{self.name}はレベルが1上がった!")
         self.level += 1
-        self.hp = HP(int(self.hp.value * 1.25))
-        self.atk = Attack(int(self.atk.value * 1.25))
+        self.max_hit_point += 2
+        self.reset()
+        self.strength += 1
+        self.defense += 1
+        self.show_stats()
+
+    def show_stats(self):
+        print(f"{self.name}(Lv:{self.level}) - HP:{self.hit_point}/{self.max_hit_point} STR:{self.strength} DEF:{self.defense}")
 
 
-class Enemy(Actor):
-    def __init__(self, name, hp, atk, exp) -> None:
-        super().__init__(name, hp, atk, exp=exp)
 
-    def show_attack_message(self, target, dmg):
-        print(self.name, "の", "こうげき")
-        print(target.name, "は", dmg, "ダメージを受けた!")
+class Party:
+    def __init__(self) -> None:
+        self.members = []
 
-    def clone(self) -> "Enemy":
-        return Enemy(self.name, self.hp, self.atk, self.exp)
+    def add(self, member):
+        self.members.append(member)
 
-    def on_turn(self, target):
-        command = 1
-        match command:
-            case 0:
-                print("にげだした!")
-                return False
-            case 1:
-                self.attack(target)
-        return True
+    def is_over(self):
+        return all([member.is_dead() for member in self.members])
 
-
-if __name__ == "__main__":
-
-    player = Player("アリス", HP(300), Attack(120))
-    enemy = Enemy("スライム", HP(180), Attack(90), 299909)
-
-    clone_player = player.clone()
-    clone_enemy = enemy.clone()
-
-    print(enemy.name,"が現れた!")
-
-    player.print_status()
-    enemy.print_status()
+    def alive_members(self, idx=True):
+        members = []
+        for i, member in enumerate(self.members):
+            if member.is_dead():
+                continue
+            if idx:
+                members.append(i)
+            else:
+                members.append(member)
+        return members
 
 
+
+class BattleMember:
+    def __init__(self, pp, ep) -> None:
+        self.player_party: Party = pp
+        self.enemy_party: Party = ep
+
+
+def battle(member: BattleMember):
+    pp = member.player_party
+    ep = member.enemy_party
+    if member is not None:
+        players = pp.members
+        enemies = ep.members
+
+    for enemy in enemies:
+        print(f"{enemy.name}があらわれた!")
+    for player in players:
+        player.show_stats()
+    for enemy in enemies:
+        enemy.show_stats()
     while True:
+        for player in players:
+            if player.is_dead():
+                continue
 
-        result = player.on_turn(enemy)
-        if not result:
+            if len(ep.alive_members(False)) > 1:
+                t = ",".join(map(str, ep.alive_members()))
+                target = int(input(f"({t})> "))
+            else:
+                target = 0
+            player.attack(enemies[target])
+
+            if enemies[target].is_dead():
+                print(enemies[target].name, "を倒した!")
+
+        if ep.is_over():
             break
 
-        if enemy.is_dead():
-            print(enemy.name,"を倒した!")
-            clone_player.earn_exp(enemy.exp)
+        for enemy in enemies:
+            if enemy.is_dead():
+                continue
+            target = random.randint(0, len(players)-1)
+            enemy.attack(players[target])
+
+        if pp.is_over():
             break
 
-        result = enemy.on_turn(player)
-        if not result:
-            break
+        for player in players:
+            player.show_stats()
 
-        if player.is_dead():
-            print(player.name,"は死んでしまった...")
-            break
+        for enemy in enemies:
+            enemy.show_stats()
 
-        player.print_status()
-        enemy.print_status()
+        print("="*20)
 
 
+    for player in players:
+        player.reset()
+        player.show_stats()
+    for enemy in enemies:
+        enemy.reset()
+        enemy.show_stats()
+
+    return member
+
+def main():
+    pp = Party()
+    pp.add( Actor("アリス", 1, 12, 6, 3))
+    ep = Party()
+    ep.add(Actor("スライムA", 1, 8, 3, 1))
+    ep.add(Actor("スライムB", 1, 8, 3, 1))
+    members = BattleMember(pp, ep)
+    battle(members)
 
 
-    player = clone_player
-    enemy = clone_enemy
-
-    player.print_status()
-    enemy.print_status()
+main()
